a6696ab2c26688a244c1a0ab16dbf9e279e51dc1
Bresneth Oliveros <boliveros18@gmail.com>
2023-06-29 22:00:18 +0000

    settings: 2th

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,3 +1,4 @@
+/*
 import { useContext, useEffect } from "react";
 import { GetServerSideProps, NextPage } from "next";
 import { dbCertifications, dbClinics, dbQualifications } from "../../database";
@@ -239,3 +240,4 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 };
 
 export default ClinicPage;
+*/
